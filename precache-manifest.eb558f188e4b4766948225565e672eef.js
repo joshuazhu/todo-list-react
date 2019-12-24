1e8d9b847983dffb81d62bde4845bbef0256268c
Josh Zhu <josh.zhu@acloud.guru>
2019-12-24 20:48:12 +1100

    Updates

---
+++ precache-manifest.eb558f188e4b4766948225565e672eef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da4b41d23a8910937c0a007ee1e1dfc4",
+    "url": "/todo-list-react/index.html"
+  },
+  {
+    "revision": "480085b55d5c2fcf080f",
+    "url": "/todo-list-react/static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "480085b55d5c2fcf080f",
+    "url": "/todo-list-react/static/js/2.62ec8322.chunk.js"
+  },
+  {
+    "revision": "a3050c59f07cab00f56a6005f5dc7d0e",
+    "url": "/todo-list-react/static/js/2.62ec8322.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23d164661c13a4c07486",
+    "url": "/todo-list-react/static/js/main.188a66c8.chunk.js"
+  },
+  {
+    "revision": "7cd9e2fa0837a6af9177",
+    "url": "/todo-list-react/static/js/runtime-main.e46d85e9.js"
+  }
+]);
