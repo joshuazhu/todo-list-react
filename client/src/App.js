09d331028b34e21ee19b26ded5d411136d5a89ed
Josh Zhu <josh.zhu@acloud.guru>
2020-01-01 23:00:36 +1100

    :sparkles: add auth0 integration

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,33 @@ import { TodoList } from './components/TodoList';
 import { TodosContext } from './context';
 import { todoReducer } from './reducer';
 import { TodoForm } from './components/TodoForm';
-
+import { Auth0Context } from './context/Auth0Context';
 
 export default function App() {
   const initialState = useContext(TodosContext);
   const [state, dispatch] = useReducer(todoReducer, initialState);
+  const useAuth0 = () => useContext(Auth0Context);
+
+  const { isAuthenticated, loginWithRedirect, loading, user } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <TodosContext.Provider value={{ state, dispatch }}>
-      <TodoForm />
-      <TodoList />
-    </TodosContext.Provider>
+    <div>
+      {!isAuthenticated &&
+        <button onClick={() => loginWithRedirect({})}>Log in</button>
+      }
+
+      {
+        isAuthenticated &&
+        <TodosContext.Provider value={{ state, dispatch }}>
+          <div>Hello {user.name}</div>
+          <TodoForm />
+          <TodoList />
+        </TodosContext.Provider>
+      }
+    </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,27 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import "antd/dist/antd.css";
+import { Auth0Provider } from "./context/Auth0Provider";
+import config from "./auth_config.json";
+import history from "./utils/history";
+
+const onRedirectCallback = appState => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
 
 ReactDOM.render(
-  <App />
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />
+  </Auth0Provider>
   , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
